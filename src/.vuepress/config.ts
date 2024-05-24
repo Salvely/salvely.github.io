12506aad4bdd4cb661b8812b1f7ec3e1bdb29b2e
Salvely <1247230776@qq.com>
2024-05-24 16:49:50 +0800

    remove plugin-docsearch, fix the search feature using plugin-searchPro

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import { getDirname, path } from "vuepress/utils";
 import { viteBundler } from "@vuepress/bundler-vite";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 // import { ohmylive2dPlugin } from "vuepress-plugin-oh-my-live2d";
 import {
   canvasPlugin,
@@ -108,54 +107,54 @@ export default defineUserConfig({
       debug: true,
     }),
     // 搜索插件
-    docsearchPlugin({
-      appId: "PI9QTF572N",
-      apiKey: "07e2432555d8ecdd3fb72978f0a05cdc",
-      indexName: "salvely",
-      locales: {
-        "/": {
-          placeholder: "搜索内容",
-          translations: {
-            button: {
-              buttonText: "搜索",
-              buttonAriaLabel: "搜索",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
-              },
-            },
-          },
-        },
-      },
-    }),
+    // docsearchPlugin({
+    //   appId: "PI9QTF572N",
+    //   apiKey: "07e2432555d8ecdd3fb72978f0a05cdc",
+    //   indexName: "salvely",
+    //   locales: {
+    //     "/": {
+    //       placeholder: "搜索内容",
+    //       translations: {
+    //         button: {
+    //           buttonText: "搜索",
+    //           buttonAriaLabel: "搜索",
+    //         },
+    //         modal: {
+    //           searchBox: {
+    //             resetButtonTitle: "清除查询条件",
+    //             resetButtonAriaLabel: "清除查询条件",
+    //             cancelButtonText: "取消",
+    //             cancelButtonAriaLabel: "取消",
+    //           },
+    //           startScreen: {
+    //             recentSearchesTitle: "搜索历史",
+    //             noRecentSearchesText: "没有搜索历史",
+    //             saveRecentSearchButtonTitle: "保存至搜索历史",
+    //             removeRecentSearchButtonTitle: "从搜索历史中移除",
+    //             favoriteSearchesTitle: "收藏",
+    //             removeFavoriteSearchButtonTitle: "从收藏中移除",
+    //           },
+    //           errorScreen: {
+    //             titleText: "无法获取结果",
+    //             helpText: "你可能需要检查你的网络连接",
+    //           },
+    //           footer: {
+    //             selectText: "选择",
+    //             navigateText: "切换",
+    //             closeText: "关闭",
+    //             searchByText: "搜索提供者",
+    //           },
+    //           noResultsScreen: {
+    //             noResultsText: "无法找到相关结果",
+    //             suggestedQueryText: "你可以尝试查询",
+    //             reportMissingResultsText: "你认为该查询应该有结果？",
+    //             reportMissingResultsLinkText: "点击反馈",
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // }),
     // 看板娘插件
     // ohmylive2dPlugin({
     //   // 在这里进行配置
